e900fa64602c4fe068a8934e35ae33d608dfeaa8
Konrad <83445742+konradrenis@users.noreply.github.com>
2023-04-04 18:39:04 +0200

    Project created

---
+++ Source/battlegame/battlegameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "battlegameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEGAME_API AbattlegameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
